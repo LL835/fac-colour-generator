744a2467d02f9d2c22def666ca73e59f284185bd
ll835 <loktinlo@gmail.com>
2022-07-06 17:51:47 +0100

    create function to toggle lock icon image

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ function copyHexToClipBoard(colour){
     navigator.clipboard.writeText(hexCode)
 }
 
+function toggleLock(button){
+    const status = button.parentElement.parentElement.parentElement
+    if (status.dataset.status === "unlocked"){
+        button.src = "./icons/lock.png";
+        status.dataset.status = "locked";
+    } else {
+        button.src = "./icons/unlock.png";
+        status.dataset.status = "unlocked"
+    }
+}
+
 // Generates a new colour palette as soon as page is loaded.
 document.addEventListener("DOMContentLoaded", generateColourPalette);
 
@@ -44,5 +55,13 @@ const copyIcons = document.querySelectorAll(".copy-icon");
 for (let i = 0; i < copyIcons.length; i++){
     copyIcons[i].addEventListener("click", (e) => {
         copyHexToClipBoard(e.target);
+    })
+}
+
+// Lets the user lock a colour
+const padlockIcons = document.querySelectorAll(".padlock-icon")
+for (let i = 0; i < padlockIcons.length; i++){
+    padlockIcons[i].addEventListener("click", (e) => {
+        toggleLock(e.target)
     })
 }
